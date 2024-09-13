779c6848687b96bb05df85a5ad5214ddbddf7f8b
HMonique <moniquehl@outlook.com>
2024-09-13 09:18:27 -0400

    first commit

---
+++ src/main/java/com/perscholas/SpringSecurity/RoleService.java
@@ -0,0 +1,13 @@
+package com.perscholas.SpringSecurity;
+
+import javax.management.relation.Role;
+import java.util.List;
+
+public interface RoleService {
+    public void saveRole(Role role);
+    public Role findRoleByRoleName(String name);
+    public List<Role> getAllRoles();
+    public List<Role> getRolesByUser(long id);
+}
+
+
